7e2fa3bc9390b68dbbfdbd5f146e057c4414b0e5
Jakob <shavinski.jakob@gmail.com>
2023-03-31 21:15:24 -0700

    revamped project section

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
+const navBar = document.querySelector('.navbar');
 
 hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
+    navBar.classList.toggle('active')
 });
 
 document.querySelectorAll('.nav-link').forEach(nav => {
     nav.addEventListener('click', function() {
         hamburger.classList.remove('active')
         navMenu.classList.remove('active')
+        navBar.classList.toggle('active')
     });
 });
